da584e884d37bcc1493897b0e33b4f280dd67c67
Mike <ms53150@gmail.com>
2022-06-04 23:53:17 -0400

    Add layouts for header and footer

--- app.js
+++ app.js
@@ -48,6 +48,10 @@ app.get("/", function (req, res) {
 	res.render("list", { kindOfDay: day });
 });
 
+app.get("/about", function (req, res) {
+	res.render("about");
+});
+
 app.listen(3000, function () {
 	console.log("Server started on port 3000");
 });
